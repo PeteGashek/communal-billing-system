a1fa7f3e9e8b86502777e75ade74d511b432863d
PeteGashek <crimon.elite@gmail.com>
2013-03-15 13:16:30 +0200

    added createYear command

--- cbs/core/src/main/java/cbs/core/TestLogic.java
+++ cbs/core/src/main/java/cbs/core/TestLogic.java
@@ -10,42 +10,41 @@ import javax.xml.bind.Marshaller;
 public class TestLogic {
 
     /**
-     * @param args
-     */
+* @param args
+*/
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-//        ElectricBill electricBill = new ElectricBill();
-//        electricBill.setCounterStatement(100);
-//        
-//        WaterBill waterBill = new WaterBill();
-//        waterBill.setCounterStatement(100.451);
-//        
-//        ElectricBill electricBill2 = new ElectricBill();
-//        electricBill2.setCounterStatement(123);
-//        
-//        WaterBill waterBill2 = new WaterBill();
-//        waterBill2.setCounterStatement(110.351);
-//        
-//        MonthBill monthBill = new MonthBill();
-//        monthBill.setMonth(Months.January);
-//        monthBill.setElectricBill(electricBill);
-//        monthBill.setWaterBill(waterBill);
-//        
-//        MonthBill monthBill2 = new MonthBill();
-//        monthBill2.setMonth(Months.January);
-//        monthBill2.setElectricBill(electricBill2);
-//        monthBill2.setWaterBill(waterBill2);
+// ElectricBill electricBill = new ElectricBill();
+// electricBill.setCounterStatement(100);
+//
+// WaterBill waterBill = new WaterBill();
+// waterBill.setCounterStatement(100.451);
+//
+// ElectricBill electricBill2 = new ElectricBill();
+// electricBill2.setCounterStatement(123);
+//
+// WaterBill waterBill2 = new WaterBill();
+// waterBill2.setCounterStatement(110.351);
+//
+// MonthBill monthBill = new MonthBill();
+// monthBill.setMonth(Months.January);
+// monthBill.setElectricBill(electricBill);
+// monthBill.setWaterBill(waterBill);
+//
+// MonthBill monthBill2 = new MonthBill();
+// monthBill2.setMonth(Months.January);
+// monthBill2.setElectricBill(electricBill2);
+// monthBill2.setWaterBill(waterBill2);
         
         YearBill yearBill = new YearBill();
         yearBill.setNumber(2013);
-//        LinkedHashSet<MonthBill> al = new  LinkedHashSet<MonthBill>();
-//        al.add(monthBill);
-//        al.add(monthBill2);
-//        yearBill.setMonths(al);
+// LinkedHashSet<MonthBill> al = new LinkedHashSet<MonthBill>();
+// al.add(monthBill);
+// al.add(monthBill2);
+// yearBill.setMonths(al);
 
         //
-
         try {
 
             // File file = new File("E:\\test.xml");
