e4eaf8209c77b218bffbbb6ac381ee7f6ca9ccc7
PeteGashek <crimon.elite@gmail.com>
2013-03-26 12:01:48 +0200

    added hashcode and equals to Bill override

--- cbs/core/src/main/java/cbs/core/model/Bill.java
+++ cbs/core/src/main/java/cbs/core/model/Bill.java
@@ -41,6 +41,28 @@ public class Bill {
 
     @Override
     public int hashCode() {
-        return date.toString().hashCode();
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((date == null) ? 0 : date.hashCode());
+        return result;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bill other = (Bill) obj;
+        if (date == null) {
+            if (other.date != null)
+                return false;
+        } else if (!date.equals(other.date))
+            return false;
+        return true;
+    }
+
+
 }
