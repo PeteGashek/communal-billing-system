f7c84d0ea7092ba113218b84a2a2c02b5ceb2312
gary <garygg42@gmail.com>
2013-03-26 11:25:14 +0200

    testing set logic

--- cbs/dao/src/main/java/cbs/dao/TestLogic.java
+++ cbs/dao/src/main/java/cbs/dao/TestLogic.java
@@ -1,85 +1,16 @@
 package cbs.dao;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class TestLogic {
 
     public static void main(String[] args) {
-        
-        
-        
-//        ApplicationContext acx = new ClassPathXmlApplicationContext("META-INF/spring/context.xml");
-//        BillDAOXmlImpl impl = (BillDAOXmlImpl) acx.getBean("billdaoxml");
-//        impl.createBill();
-//        impl.createBill(new YearMonth(2015, 6));
-        
-        
-        
-        
-        
-        
-        
-//        Bills bills = new Bills();
-//
-//        bills.put("electricity", CountableService.class);
-//        bills.put("hotWater", CountableService.class);
-//        bills.put("coldWater", CountableService.class);
-//        bills.put("kvartplata", UncountableService.class);
-//        bills.put("internet", UncountableService.class);
-//
-//        List<Service> servicesList = new ArrayList<Service>();
-//        for (Entry<String, Class<? extends Service>> entry : bills
-//                .getServices().entrySet()) {
-//            Service temp = (Service) entry.getValue().newInstance();
-//            temp.setServiceName(entry.getKey());
-//            if (temp instanceof CountableService) {
-//                ((CountableService) temp).setCounter(999);
-//            }
-//            servicesList.add(temp);
-//        }
-//
-//        Bill bill = new Bill();
-//        bill.setDate(new YearMonth());
-//        bill.setServices(servicesList);
-//
-//        bills.add(bill);
-//        ApplicationContext acx = new ClassPathXmlApplicationContext("META-INF/spring/context.xml");
-////        Config conf = (Config)acx.getBean("config");
-////        System.out.println(conf.getBillsPath());
-//        try {
-//
-//            File file = new File("test.xml");
-//            JAXBContext jaxbContext = JAXBContext.newInstance(Bills.class);
-//            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-//
-//            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//            jaxbMarshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "");
-//
-//            jaxbMarshaller.marshal(bills, file);
-//            jaxbMarshaller.marshal(bills, System.out);
-//
-//        } catch (JAXBException e) {
-//            e.printStackTrace();
-//        }
-//
-//        System.out.println("============================");
-//
-//        try {
-//
-//            File file = new File("test.xml");
-//            JAXBContext jaxbContext = JAXBContext.newInstance(Bills.class);
-//            Unmarshaller jaxbUnMarshaller = jaxbContext.createUnmarshaller();
-//            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-//
-//            Bills unmarshalTest = (Bills) jaxbUnMarshaller.unmarshal(file);
-//
-//            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-//            jaxbMarshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "");
-//
-//            jaxbMarshaller.marshal(unmarshalTest, System.out);
-//
-//        } catch (JAXBException e) {
-//            e.printStackTrace();
-//        }
-
+        Set<String> set = new HashSet<String>();
+        System.out.println(set.add("aaa"));
+        System.out.println(set.add("bbb"));
+        System.out.println(set.add("aaa"));
+        System.out.println(set);
     }
 }
